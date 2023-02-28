9469223ea63735f267d19a225df5d2eaa57dc87d
luis-moguea <luis.mogueadp@gmail.com>
2023-02-27 19:41:59 -0500

    updating express request

--- routes/appRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.router = void 0;
-var express_1 = require("express");
-var appController_1 = require("../controllers/appController");
-exports.router = (0, express_1.Router)();
-exports.router.get("/api/citizens", appController_1.controller.getCitizens);
-exports.router.get("/api/citizens/:id", appController_1.controller.getCitizen);
-console.log(exports.router);
